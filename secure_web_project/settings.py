568c1eefeb000e2488a9bcd8f6f03622686c7391
hunyoungcha <chamgf5247@naver.com>
2024-12-08 19:41:41 +0900

    create link 수정

--- secure_web_project/settings.py
+++ secure_web_project/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-# Default primary key field type
+# Default primary  key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
